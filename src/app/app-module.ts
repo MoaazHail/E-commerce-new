b90f5f283dd6e026d0af2f529ecf119e7837978a
Moaaz Hail <hailmoaaz@gmail.com>
2026-03-08 17:05:18 +0200

    config: Create A Folder Structure And Install A Bootstarp

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { CoreModule } from './core/core-module';
+import { SharedModule } from './shared/shared-module';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
