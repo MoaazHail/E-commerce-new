3469478a792c99b4bf7a893aa9ac41109fa7b588
Moaaz Hail <hailmoaaz@gmail.com>
2026-03-11 11:09:40 +0200

    feat: Error Handling And API Error Meaasge

--- src/app/core/auth/login/login.ts
+++ src/app/core/auth/login/login.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,11 +15,17 @@ export class Login implements OnInit {
   // Forms
   loginForm: FormGroup;
 
+  //
+  apiError: string = '';
+
   ngOnInit(): void {
     this.onSubmit();
   }
 
-  constructor(private loginService: AuthService) {
+  constructor(
+    private loginService: AuthService,
+    private router: Router,
+  ) {
     this.loginForm = new FormGroup({
       username: new FormControl('', [Validators.required]),
       password: new FormControl('', [Validators.required, Validators.minLength(8)]),
@@ -33,10 +40,12 @@ export class Login implements OnInit {
     if (values.password !== '') {
       this.loginService.login(values).subscribe({
         next: (data) => {
-          console.log(data);
+          localStorage.setItem('token', data.accessToken);
+          this.router.navigate(['/']);
         },
-        error(err) {
+        error: (err) => {
           console.log(err);
+          this.apiError = err?.error?.message;
         },
       });
     }
