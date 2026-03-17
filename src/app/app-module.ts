1e233c31ab40437f027e266f76dafcddecac53fc
Moaaz Hail <hailmoaaz@gmail.com>
2026-03-17 13:03:57 +0200

    Feat: Create A Home Page And Configrate A ngx-toastr

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -6,11 +6,22 @@ import { App } from './app';
 import { CoreModule } from './core/core-module';
 import { SharedModule } from './shared/shared-module';
 import { AuthModule } from './core/auth/auth-module';
+import { FeaturesModule } from './features/features-module';
+import { provideToastr } from 'ngx-toastr';
 
 @NgModule({
   declarations: [App],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule, AuthModule],
-  providers: [provideBrowserGlobalErrorListeners()],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule, AuthModule, FeaturesModule],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    // For animations ??
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      progressBar: true,
+    }),
+  ],
   bootstrap: [App],
 })
 export class AppModule {}
