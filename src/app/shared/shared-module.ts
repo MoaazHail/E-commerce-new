103031035fc460bdc17948ae63c059db7448e5a0
Moaaz Hail <hailmoaaz@gmail.com>
2026-03-17 13:33:02 +0200

    fix: shared module

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputField } from './input/input-field';
-import { LucideAngularModule, EyeOff, Eye, Inbox, KeyRound, AtSign } from 'lucide-angular';
+import { LucideAngularModule, EyeOff, Eye, Inbox, KeyRound, AtSign, LogOut } from 'lucide-angular';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Navbar } from './layout/navbar/navbar';
+import { AppRoutingModule } from '../app-routing-module';
 
 @NgModule({
-  declarations: [InputField],
+  declarations: [InputField, Navbar],
   imports: [
     CommonModule,
-    LucideAngularModule.pick({ EyeOff, Eye, Inbox, KeyRound, AtSign }),
+    LucideAngularModule.pick({ EyeOff, Eye, Inbox, KeyRound, AtSign, LogOut }),
     ReactiveFormsModule,
+    AppRoutingModule,
   ],
   exports: [InputField],
 })
